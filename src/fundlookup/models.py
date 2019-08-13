83421c411db18db54a7710a0e875c8419da6f444
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2019-08-14 02:23:21 +0530

    Adding the Party database and populating it with two parties

--- src/fundlookup/models.py
+++ src/fundlookup/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Party(models.Model):
+    name    = models.CharField("Name", max_length=60)
+    address = models.CharField("Address", max_length=500)
