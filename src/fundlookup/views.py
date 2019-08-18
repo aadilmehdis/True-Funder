490d85760f6ef246cb3f278bdbd5f0f4d6dccfe7
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2019-08-18 20:05:54 +0530

    Linking backend and frontend for landing page

--- src/fundlookup/views.py
+++ src/fundlookup/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Party
 
 def index(request):
 
+    all_parties = Party.objects.all()
+    for p in all_parties:
+        print(p.symbol)
     context = {
-
-
+        'all_parties': all_parties
     }
     return render(request, 'home/index.html', context)
 
