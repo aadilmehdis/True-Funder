b9b2a2ec4943cc1b1bbdd7952e3eafb40383236c
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2019-08-18 19:21:09 +0530

    Updating the Databse

--- src/fundlookup/models.py
+++ src/fundlookup/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Party(models.Model):
-    name    = models.CharField("Name", max_length=60)
+    name    = models.CharField("Name", max_length=120)
     address = models.CharField("Address", max_length=500)
+    symbol  = models.CharField("Symbol", max_length=500,null=True)
--- src/fundlookup/views.py
+++ src/fundlookup/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-
     return render(request, 'home/index.html')
