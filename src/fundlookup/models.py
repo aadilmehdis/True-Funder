0ff81468b9482ff69fcb3687ec99ba8de1dade70
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2020-02-04 14:42:54 +0530

    Adding the new models

--- src/fundlookup/models.py
+++ src/fundlookup/models.py
@@ -1,7 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user                = models.OneToOneField(User)
+
+    USER_TYPE           = [
+        ('VR', "Vendor"),
+        ('FD', "Funder"),
+        ('PP', "Political Party"),
+        ('AD', "Admin")
+    ]
+    user_type           = models.CharField(
+        max_length=2,
+        choices=USER_TYPE,
+        default="FD",
+    )
+
+    avatar_path         = models.CharField("Avatar Path", max_length=500)
+    metamask_address    = models.CharField("Metamask Address", max_length=500)
+    available_funds     = models.FloatField()
+    amount_requested    = models.FloatField()
+
+class Funder(models.Model):
+    user                = models.OneToOneField(User)
+
+    FUNDER_TYPE         = [
+        ("IND", "Individual"),
+        ("ORG", "Organisation"),
+    ]
+    funder_type         = models.CharField(
+        max_length=3,
+        choices=FUNDER_TYPE,
+        default="IND",
+    )
+
+class Vendor(models.Model):
+    user                = models.OneToOneField(User)
+
+    VENDOR_TYPE         = [
+        ("PB", "Publicity"),
+        ("EV", "Event"),
+        ("TR", "Travel"),
+        ("MC", "Miscellaneous"),
+    ]
+    vendor_type         = models.CharField(
+        max_length=2,
+        choices=VENDOR_TYPE,
+        default="MC",
+    )
+
+class Party(models.Model):
+    user                = models.OneToOneField(User)
+
+    PARTY_TYPE         = [
+        ("RG", "Regional"),
+        ("NT", "National"),
+    ]
+    party_type         = models.CharField(
+        max_length=2,
+        choices=PARTY_TYPE,
+        default="NT",
+    )
+
+class Admin(models.Model):
+    user                = models.OneToOneField(User)
+
+
+class Transactions(models.Model):
+    donated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    donated_to = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Party(models.Model):
     name    = models.CharField("Name", max_length=120)
     address = models.CharField("Address", max_length=500)
     symbol  = models.CharField("Symbol", max_length=500,null=True)
+
